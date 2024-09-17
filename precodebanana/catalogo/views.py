a2f2a8d70e2a11581bab9cb71b9d4753cc9784bc
AlanBMC <alanbmc00@gmail.com>
2024-09-17 08:43:07 -0400

    feat: criando estrutura do projeto - back-end

--- precodebanana/catalogo/views.py
+++ precodebanana/catalogo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def catalogo(request):
+    pass
+
+def carrinho(request):
+    pass
+
+def envia_mensagem_wpp(request):
+    pass
+
