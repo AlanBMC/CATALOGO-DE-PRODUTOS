fd057d3e06187f681022a141f7581591d7a32100
AlanBMC <alanbmc00@gmail.com>
2024-09-17 10:17:24 -0400

    feat: adicionando arquivos e base de dados

--- precodebanana/catalogo/models.py
+++ precodebanana/catalogo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    imagem = models.ImageField(upload_to='produtos/')
+    preco_un = models.DecimalField(max_digits=10, decimal_places=2)
+    preco_por_caixa = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade_na_caixa = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Carrinho(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    cep = models.CharField(max_length=9)
+    quantidade = models.IntegerField()
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def calcular_valor_total(self):
+        self.valor_total = self.quantidade * self.produto.preco_un
+        return self.valor_total
